e7040447389c6123ddbd5c6d7a2efd6f98746d91
Evan John Gould <dcentauri@gmail.com>
2018-02-10 18:52:07 -0500

    add license and readme file

--- src/main/kotlin/io/github/lenois/geonames/WebService.kt
+++ src/main/kotlin/io/github/lenois/geonames/WebService.kt
@@ -1,4 +1,18 @@
 package io.github.lenois.geonames
 
-interface WebService {
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.httpGet
+
+abstract class WebService<out T: Response>(
+        val userName: String
+) {
+    init {
+        FuelManager.instance.basePath = "api.geonames.org"
+    }
+    fun search(): T{
+        "/search".httpGet(listOf(
+
+        ))
+    }
 }
+
