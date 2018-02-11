cdcb28ede24a6ff4165620daa7b0a20448c29cb6
Lenois <dcentauri@gmail.com>
2018-02-11 15:56:48 -0500

    Update WebServices to be implemented

--- src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
+++ src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
@@ -1,10 +1,18 @@
 package io.github.lenois.geonames
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.jackson.jacksonDeserializerOf
 
 class ShortResponseWebService(userName: String) : AbstractWebService<ShortToponym>(userName, ResponseStyle.SHORT) {
-    override fun getDeserializer(): ResponseDeserializable<ShortToponym> {
+    override fun getSearchDeserializer(): ResponseDeserializable<SearchResultWrapper<ShortToponym>> {
         return jacksonDeserializerOf()
     }
+}
+
+fun main(args: Array<String>) {
+    ShortResponseWebService("lenois").search(SearchRequest("germany", SearchRequest.QueryType.ALL_ATTRIBUTES))
 }
--- src/main/kotlin/io/github/lenois/geonames/ShortToponym.kt
+++ src/main/kotlin/io/github/lenois/geonames/ShortToponym.kt
@@ -1,4 +1,17 @@
 package io.github.lenois.geonames
 
-class ShortToponym :Toponym{
-}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ShortToponym(
+        val toponymName: String?,
+        val name: String?,
+        val lat: Double?,
+        val lng: Double?,
+        val geonameId: Int?,
+        val countryCode: String?,
+        val fcl: String?,
+        val fcode: String?
+) :Toponym
