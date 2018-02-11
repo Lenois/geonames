47ed2888eb8a98ea557b8d9763008d5481cda54d
Lenois <dcentauri@gmail.com>
2018-02-11 16:15:10 -0500

    Merge branch 'master' of github.com:Lenois/geonames
    # Conflicts:
    #	src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
    #	src/test/kotlin/io/github/lenois/geonames/ShortResponseWebServiceSpec.kt

--- src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
+++ src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.github.lenois.geonames
 
 import com.github.kittinunf.fuel.core.ResponseDeserializable
@@ -9,23 +8,4 @@ class ShortResponseWebService(userName: String) : AbstractWebService<ShortTopony
         return jacksonDeserializerOf()
     }
 }
-=======
-package io.github.lenois.geonames
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.github.kittinunf.fuel.core.ResponseDeserializable
-import com.github.kittinunf.fuel.jackson.jacksonDeserializerOf
-
-class ShortResponseWebService(userName: String) : AbstractWebService<ShortToponym>(userName, ResponseStyle.SHORT) {
-    override fun getSearchDeserializer(): ResponseDeserializable<SearchResultWrapper<ShortToponym>> {
-        return jacksonDeserializerOf()
-    }
-}
-
-fun main(args: Array<String>) {
-    ShortResponseWebService("lenois").search(SearchRequest("germany", SearchRequest.QueryType.ALL_ATTRIBUTES))
-}
->>>>>>> cdcb28ede24a6ff4165620daa7b0a20448c29cb6
