9380ef573d537f258a5838075f0932dfa5f39e5f
Evan John Gould <dcentauri@gmail.com>
2018-02-11 16:11:01 -0500

    update short response webService

--- src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
+++ src/main/kotlin/io/github/lenois/geonames/ShortResponseWebService.kt
@@ -1,4 +1,10 @@
 package io.github.lenois.geonames
 
-class ShortResponseWebService {
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.jackson.jacksonDeserializerOf
+
+class ShortResponseWebService(userName: String) : AbstractWebService<ShortToponym>(userName, ResponseStyle.SHORT) {
+    override fun getSearchDeserializer(): ResponseDeserializable<SearchResultWrapper<ShortToponym>> {
+        return jacksonDeserializerOf()
+    }
 }
